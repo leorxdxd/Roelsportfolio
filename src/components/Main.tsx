06efd87df6c642d92f02b4f30f908f5fae67b23a
Roel Delos Reyes <123723123+leorxdxd@users.noreply.github.com>
2025-01-22 11:51:45 +0800

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -34,7 +34,7 @@ function Main() {
             I'm <span className="highlight">Roel Delos Reyes</span>
           </h1>
           <p className="profession">
-            <span className="animated-profession">This is not yet Final. Thank you! >_<</span>
+            <span className="animated-profession">This is not yet Final</span>
           </p>
           <div className="action-buttons">
             <a
